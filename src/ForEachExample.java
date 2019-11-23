df32a423ff3c818976f28eddd83a5835696a89fb
Kumar <kumarvadu@gmail.com>
2019-11-23 11:37:32 -0500

    first commit

---
+++ src/ForEachExample.java
@@ -0,0 +1,14 @@
+public class ForEachExample {
+
+    public static void main(String[] args) {
+
+        int arr[]={12,13,14,44};
+        //traversing the array with for-each loop
+        for(int i:arr){
+            if(i>20)
+            System.out.println(i);
+        }
+    }
+
+
+}
